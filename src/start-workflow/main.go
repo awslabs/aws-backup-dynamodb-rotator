9dc92e824c30ddbd90530508e0b5015d239085f0
Rob Sutter <robsut@amazon.com>
2019-09-29 10:35:36 +0400

    Added check for DynamoDB resource type.

--- src/start-workflow/main.go
+++ src/start-workflow/main.go
@@ -129,18 +129,31 @@ func isMatchingJob(message BackupSnsMessage, matchString string) (bool, error) {
 	// Check to see if we have a match on the input
 	// Match requires:
 	//   1. this is a BACKUP_JOB_COMPLETE event
-	//   2. the underlying resource matches our SOURCE_PATTERN regexp pattern
+	//   2. the underlying resource is a DynamoDB table
+	//   3. the underlying resource matches our SOURCE_PATTERN regexp pattern
 
-	// 1. This must be a BACKUP_JOB_COMPLETE event. This is only be determined by
-	//    matching on the string ""
+	// 1. This must be a BACKUP_JOB_COMPLETE event. Currently this can be determined by
+	//    matching on the string "An AWS Backup job was completed successfully."
 	//    in the SNS message.
-	const searchString = "An AWS Backup job was completed successfully."
-	if !strings.Contains(message.StatusMessage, searchString) {
+	const successfulBackupString = "An AWS Backup job was completed successfully."
+	if !strings.Contains(message.StatusMessage, successfulBackupString) {
 		fmt.Printf("This was not a BACKUP_COMPLETED job: %v\n", message.StatusMessage)
 		return false, nil
 	}
 
-	// 2. The underlying resource matches our SOURCE_PATTERN regexp pattern.
+	// 2. The backed up resource must be a DynamoDB table.
+	const dynamoTableExpression = "(?i)^arn:aws:dynamodb:.*:.*:table/.*"
+	isDynamoTable, err := regexp.MatchString(dynamoTableExpression, message.BackedUpResourceArn)
+	if err != nil {
+		fmt.Printf("Error matching backed up resource to DynamoDB regular expression: %s\n", message.BackedUpResourceArn)
+		return false, err
+	}
+	if isDynamoTable == false {
+		fmt.Printf("The backed up resource was not a DynamoDB table: %s\n", message.BackedUpResourceArn)
+		return false, nil
+	}
+
+	// 3. The underlying resource matches our SOURCE_PATTERN regexp pattern.
 	//    MatchString reports whether the string s contains any match of the regular
 	//    expression pattern. More complicated queries need to use Compile and the
 	//    full Regexp interface.
